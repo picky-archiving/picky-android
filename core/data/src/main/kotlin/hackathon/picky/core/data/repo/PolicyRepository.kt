147eddac251a8b38892373b7272952e703f34ba8
keem-hyun <hw813@naver.com>
2025-11-23 03:17:27 +0900

    feat: 전체 정책 목록 조회 - repository & impl

--- core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepository.kt
+++ core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepository.kt
@@ -1,8 +1,17 @@
 package hackathon.picky.core.data.repo
 
 import hackathon.picky.core.network.model.response.PolicyDetailData
+import hackathon.picky.core.network.model.response.PolicyPageData
 
 interface PolicyRepository {
+    /**
+     * 전체 게시물 목록 조회
+     * @param page 페이지 번호 (0-base)
+     * @param size 페이지 크기
+     * @return Result<PolicyPageData> 성공 시 페이징된 정책 목록, 실패 시 에러
+     */
+    suspend fun getPolicyList(page: Int, size: Int): Result<PolicyPageData>
+
     /**
      * 정책 상세 정보 조회
      * @param policyId 정책 ID
--- core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepositoryImpl.kt
+++ core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepositoryImpl.kt
@@ -5,11 +5,20 @@ import hackathon.picky.core.network.datasource.policy.PolicyDatasource
 import hackathon.picky.core.network.di.FakeDataSource
 import hackathon.picky.core.network.di.RealDataSource
 import hackathon.picky.core.network.model.response.PolicyDetailData
+import hackathon.picky.core.network.model.response.PolicyPageData
 import javax.inject.Inject
 
 class PolicyRepositoryImpl @Inject constructor(
     @param:RealDataSource private val policyDatasource: PolicyDatasource
 ) : PolicyRepository {
+    override suspend fun getPolicyList(page: Int, size: Int): Result<PolicyPageData> {
+        return policyDatasource.getPolicyList(page, size).toResult(
+            transform = { response ->
+                response.data
+            }
+        )
+    }
+
     override suspend fun getPolicyDetail(policyId: Long): Result<PolicyDetailData> {
         return policyDatasource.getPolicyDetail(policyId).toResult(
             transform = { response ->
