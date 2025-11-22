d521019192f9db312b4ab40b5d3daad2c96720d0
keem-hyun <hw813@naver.com>
2025-11-23 02:59:33 +0900

    feat: 소득분위 수정 - remote data source

--- core/network/src/main/kotlin/hackathon/picky/core/network/datasource/user/RetrofitUserDatasource.kt
+++ core/network/src/main/kotlin/hackathon/picky/core/network/datasource/user/RetrofitUserDatasource.kt
@@ -2,6 +2,7 @@ package hackathon.picky.core.network.datasource.user
 
 import hackathon.picky.core.network.api.UserApi
 import hackathon.picky.core.network.model.ApiResponse
+import hackathon.picky.core.network.model.request.UpdateIncomeBracketRequest
 import hackathon.picky.core.network.model.response.UserIncomeBracketResponse
 import hackathon.picky.core.network.util.runRemote
 import javax.inject.Inject
@@ -13,5 +14,17 @@ class RetrofitUserDatasource @Inject constructor(
 ) : UserDatasource {
     override suspend fun getUserIncomeBracket(userId: Long): ApiResponse<UserIncomeBracketResponse> {
         return runRemote { userApi.getUserIncomeBracket(userId) }
+    }
+
+    override suspend fun updateUserIncomeBracket(
+        userId: Long,
+        incomeBracket: Int
+    ): ApiResponse<UserIncomeBracketResponse> {
+        return runRemote {
+            userApi.updateUserIncomeBracket(
+                userId = userId,
+                request = UpdateIncomeBracketRequest(incomeBracket = incomeBracket)
+            )
+        }
     }
 }
