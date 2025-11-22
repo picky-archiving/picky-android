0f1afb71420982db77101c32d3cf755d6e689971
keem-hyun <hw813@naver.com>
2025-11-23 02:59:01 +0900

    feat: 소득분위 수정

--- core/network/src/main/kotlin/hackathon/picky/core/network/api/UserApi.kt
+++ core/network/src/main/kotlin/hackathon/picky/core/network/api/UserApi.kt
@@ -1,8 +1,11 @@
 package hackathon.picky.core.network.api
 
+import hackathon.picky.core.network.model.request.UpdateIncomeBracketRequest
 import hackathon.picky.core.network.model.response.UserIncomeBracketResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserApi {
@@ -13,5 +16,16 @@ interface UserApi {
     @GET("/api/user/{userId}")
     suspend fun getUserIncomeBracket(
         @Path("userId") userId: Long = 1
+    ): Response<UserIncomeBracketResponse>
+
+    /**
+     * 사용자 소득분위 수정
+     * @param userId 사용자 식별 ID (default = 1)
+     * @param request 수정할 소득분위 정보
+     */
+    @POST("/api/user/{userId}")
+    suspend fun updateUserIncomeBracket(
+        @Path("userId") userId: Long = 1,
+        @Body request: UpdateIncomeBracketRequest
     ): Response<UserIncomeBracketResponse>
 }
