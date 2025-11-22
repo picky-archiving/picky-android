705d77ac94add39cb19e76addbde4411d46449aa
keem-hyun <hw813@naver.com>
2025-11-23 03:27:09 +0900

    feat: 메인 화면 조회 - remote data source

--- core/network/src/main/kotlin/hackathon/picky/core/network/datasource/policy/RetrofitPolicyDatasource.kt
+++ core/network/src/main/kotlin/hackathon/picky/core/network/datasource/policy/RetrofitPolicyDatasource.kt
@@ -2,6 +2,7 @@ package hackathon.picky.core.network.datasource.policy
 
 import hackathon.picky.core.network.api.PolicyApi
 import hackathon.picky.core.network.model.ApiResponse
+import hackathon.picky.core.network.model.response.HomeResponse
 import hackathon.picky.core.network.model.response.PolicyDetailResponse
 import hackathon.picky.core.network.model.response.PolicyListResponse
 import hackathon.picky.core.network.util.runRemote
@@ -12,6 +13,10 @@ import javax.inject.Singleton
 class RetrofitPolicyDatasource @Inject constructor(
     private val policyApi: PolicyApi
 ) : PolicyDatasource {
+    override suspend fun getHomeData(): ApiResponse<HomeResponse> {
+        return runRemote { policyApi.getHomeData() }
+    }
+
     override suspend fun getPolicyList(page: Int, size: Int): ApiResponse<PolicyListResponse> {
         return runRemote { policyApi.getPolicyList(page, size) }
     }
