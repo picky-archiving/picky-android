252ce897b42700d4de62023c90b11632b16be173
keem-hyun <hw813@naver.com>
2025-11-23 03:26:34 +0900

    feat: 메인 화면 조회

--- core/network/src/main/kotlin/hackathon/picky/core/network/api/PolicyApi.kt
+++ core/network/src/main/kotlin/hackathon/picky/core/network/api/PolicyApi.kt
@@ -1,5 +1,6 @@
 package hackathon.picky.core.network.api
 
+import hackathon.picky.core.network.model.response.HomeResponse
 import hackathon.picky.core.network.model.response.PolicyDetailResponse
 import hackathon.picky.core.network.model.response.PolicyListResponse
 import retrofit2.Response
@@ -8,6 +9,14 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PolicyApi {
+    /**
+     * 메인 화면 조회
+     * - 카테고리별 정책 목록
+     * - 소득분위 기반 추천 정책 목록
+     */
+    @GET("api/home")
+    suspend fun getHomeData(): Response<HomeResponse>
+
     /**
      * 전체 게시물 목록 조회
      * @param page 페이지 번호 (0-base, default = 0)
