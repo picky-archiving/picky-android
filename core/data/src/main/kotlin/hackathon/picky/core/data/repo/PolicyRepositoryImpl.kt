fd99b35d5cc033ef6cc2dd35becb99c6adf81ae9
keem-hyun <hw813@naver.com>
2025-11-23 02:33:45 +0900

    refactor: fake -> real

--- core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepositoryImpl.kt
+++ core/data/src/main/kotlin/hackathon/picky/core/data/repo/PolicyRepositoryImpl.kt
@@ -3,11 +3,12 @@ package hackathon.picky.core.data.repo
 import hackathon.picky.core.data.mapper.toResult
 import hackathon.picky.core.network.datasource.policy.PolicyDatasource
 import hackathon.picky.core.network.di.FakeDataSource
+import hackathon.picky.core.network.di.RealDataSource
 import hackathon.picky.core.network.model.response.PolicyDetailData
 import javax.inject.Inject
 
 class PolicyRepositoryImpl @Inject constructor(
-    @param:FakeDataSource private val policyDatasource: PolicyDatasource
+    @param:RealDataSource private val policyDatasource: PolicyDatasource
 ) : PolicyRepository {
     override suspend fun getPolicyDetail(policyId: Long): Result<PolicyDetailData> {
         return policyDatasource.getPolicyDetail(policyId).toResult(
